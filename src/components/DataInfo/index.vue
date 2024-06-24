3e750b71000167ac58cffc7412c3d09655690ddd
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2024-06-24 17:48:35 +0800

    添加部分组件类型注解，阻止信息展示盒子在屏幕宽度不够时单列展示

--- src/components/DataInfo/index.vue
+++ src/components/DataInfo/index.vue
@@ -21,4 +21,9 @@ console.log(1);
   height: 100%;
   flex-wrap: wrap;
 }
+.container > * {
+  flex: 1 1 50%; /* 设置每个子元素的宽度为 50% */
+  box-sizing: border-box; /* 包括边框和内边距在宽度内 */
+  padding: 10px; /* 内边距，可以根据需要调整 */
+}
 </style>
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,16 @@
 import BorderBox from "@/components/BorderBox";
 import SvgIcon from "@/components/SvgIcon";
 import DynamicColumnar from "@/components/DynamicColumnar";
+import DataInfo from "@/components/DataInfo";
+import DataInfoItem from "@/components/DataInfoItem";
 
 declare module "vue" {
   export interface GlobalComponents {
     BorderBox: typeof BorderBox;
     SvgIcon: typeof SvgIcon;
     DynamicColumnar: typeof DynamicColumnar;
+    DataInfo: typeof DataInfo;
+    DataInfoItem: typeof DataInfoItem;
   }
 }
 export {};
